2774c63aa451c90995f434c993378af2c3ddbc0d
Larry Zhang <larryzhang101@gmail.com>
2015-10-31 19:10:42 -0400

    Changed root run.py to symlink to controllers/run.py

--- run.py
+++ run.py
@@ -1,14 +1,1 @@
-from flask import Flask, request, redirect
-import twilio.twiml
-
-app = Flask(__name__)
- 
-@app.route("/", methods=['GET', 'POST'])
-def run():
-
-    resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
-    return str(resp)
- 
-if __name__ == "__main__":
-    app.run(debug=True)
+controllers/run.py
