908c4cef8af46f8983c1ec23dfc258257b7aa245
Larry Zhang <larryzhang101@gmail.com>
2015-10-31 21:12:40 -0400

    blah

--- controllers/run.py
+++ controllers/run.py
@@ -1,12 +1,15 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session
+import views
 import twilio.twiml
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
 
+def run():
     resp = twilio.twiml.Response()
+
+    if
     with resp.message("Hello, Mobile Monkey") as m:
         m.media('http://demo.twilio.com/owl.png')
     return str(resp)
